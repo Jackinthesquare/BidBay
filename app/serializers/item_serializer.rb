4537314e7fdc79aefc661f7144d8dab7b7b24a45
Jacky Liang <asheriffy@gmail.com>
2022-06-08 11:55:48 -0400

    added first image to item index serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :item_price, :description, :item_tag, :user_id
+  attributes :id, :title, :item_price, :description, :item_tag, :user_id, :image
+
+  def image
+    ActiveModel::SerializableResource.new(object.images.first,  each_serializer: ImageSerializer)
+  end
 end
