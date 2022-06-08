4e593756d8b86562a683357a0eb3fd3c7a24b976
Jacky Liang <asheriffy@gmail.com>
2022-06-08 12:17:00 -0400

    set up GET request to fetch homepage data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,21 @@
 import NavBar from "./NavBar"
+import { useState, useEffect } from "react"
 
 const Home = () => {
+    const [items,setItems] = useState([])
+    useEffect (() => {
+        (async () => {
+            let req = await fetch('http://localhost:3000/items')
+            let res = await req.json()
+            setItems(res)
+        }) ()
+    },[items])
+    
     return (
         <div className="Home">
             <h1>hello from home!</h1>
             <NavBar />
+
         </div>
     );
 }
