a953aa2bc4c302509b60c44ebb361457109562f2
Jacky Liang <asheriffy@gmail.com>
2022-06-09 12:52:16 -0400

    redirecting solved

---
+++ client/src/components/Item.js
@@ -0,0 +1,23 @@
+import { useHistory } from 'react-router-dom'
+
+const Item = ({item, itemId, setItemId}) => {
+    const history = useHistory()
+
+    const handleRedirect = () => {
+        setItemId(item.id)
+        console.log(item.id)
+        // history.push(`/signup`) // tested routing works
+        history.push(`/item/${itemId}`)
+    }
+
+    return (
+        <div className="pic-card">
+        <h4 className="item-title">{item.title}</h4>
+        <img className="pic-card-img" src={item.image.url} onClick={handleRedirect} />
+        <p>{item.description}</p>
+        <p>{item.price} <span>Time left : </span></p>
+        </div>
+      );
+    }
+    
+    export default Item;
--- client/src/components/ItemContainer.js
+++ client/src/components/ItemContainer.js
@@ -1,17 +1,12 @@
 import "./css/ItemContainer.css"
 import { useState } from "react"
-import { useHistory } from 'react-router-dom'
+
+import Item from "./Item"
+
 
 const ItemContainer = ({ items }) => {
     const [itemId,setItemId] = useState(0)
-    const history = useHistory()
-
-    const handleRedirect = () => {
-        setItemId(item.id)
-        console.log(item.id)
-        // history.push(`/signup`) // tested routing works
-        history.push(`/item${itemID}`)
-    }
+    
     return (
         <div>
             <div className="pic-container">
@@ -19,13 +14,13 @@ const ItemContainer = ({ items }) => {
                     items.map((item) => {
                         
                         return (
-                            <div className="pic-card">
-                            <h4 className="item-title">{item.title}</h4>
-                            <img className="pic-card-img" src={item.image.url} onClick={handleRedirect} />
-                            <p>{item.description}</p>
-                            <p>{item.price} <span>Time left : </span></p>
-                        
-                            </div>
+                            // <div className="pic-card">
+                            // <h4 className="item-title">{item.title}</h4>
+                            // <img className="pic-card-img" src={item.image.url} onClick={() => handleRedirect} />
+                            // <p>{item.description}</p>
+                            // <p>{item.price} <span>Time left : </span></p>
+                            // </div>
+                            <Item key={item.id} item={item} itemId={itemId} setItemId={setItemId}/>
                         )
                     })
                 }
