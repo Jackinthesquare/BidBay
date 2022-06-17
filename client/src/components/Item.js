118c9e02da0029c9d3875e6796242be5b7b85047
Jacky Liang <asheriffy@gmail.com>
2022-06-17 16:35:50 -0400

    hid overflow image scrollbars. updated current bid with $ sign

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -15,7 +15,7 @@ const Item = ({ item, setItemId }) => {
       <h1 className="item-title">{item.title}</h1>
       <img className="pic-card-img" src={item.image.url} onClick={handleRedirect} />
       <p><span>Description : </span>{item.description}</p>
-      <p><span>Current Bid : </span> {item.item_price}</p>
+      <p><span>Current Bid : $</span> {item.item_price}</p>
       <p><span>Time Ends : </span> {item.end_time_str}</p>
       {/* <Link to={`/items/${item.id}`}>items link</Link> */}
 
